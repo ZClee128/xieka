82cac088ab7b6ad74ed94e8eba018fe74685e084
zclee <leecong@miaoshitech.com>
2025-12-17 17:45:14 +0800

    fix

--- xieka/xiekaApp.swift
+++ xieka/xiekaApp.swift
@@ -14,6 +14,7 @@ struct xiekaApp: App {
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .preferredColorScheme(.light)
                 .environmentObject(store)
         }
     }
